5656720f9d1f0dfdd162643ae86f81788fff4ee4
nguye <nguyenminhquan20403@gmail.com>
2023-12-26 23:30:18 +0700

    Quan-commit

---
+++ DataAccess/classCongTy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class classCongTy
+    {
+        public int Id { get; set; }
+        public string MaCongTy { get; set; }
+        public string TenCongTy { get; set; }
+        public string DienThoai { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string DiaChi { get; set; }
+        public classCongTy()
+        {
+
+        }
+
+    }
+}
