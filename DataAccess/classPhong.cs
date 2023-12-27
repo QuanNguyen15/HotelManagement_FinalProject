2160f102515fdd3dd70bd9c859765056294fd080
viet <vietddls06@gmail.com>
2023-12-27 16:52:50 +0700

    viet 4:53 2023

---
+++ DataAccess/classPhong.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class classPhong
+    {
+        public int id { get; set; }
+        public string tenPhong { get; set; }
+
+        public bool trangThai { get; set; }
+        public int idLoaiPhong { get; set; }
+
+        public classPhong()
+        {
+
+        }
+    }
+}
