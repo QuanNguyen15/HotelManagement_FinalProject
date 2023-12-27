7f88a6d22fc99624d469eef7db3866a79b6abc0f
nguye <nguyenminhquan20403@gmail.com>
2023-12-27 16:24:24 +0700

    Quan-commit2

---
+++ DataAccess/classTaiKhoan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class classTaiKhoan
+    {
+        public int id { get; set; }
+        public string userName {  get; set; }
+        public string password { get; set; }
+        public string maCongTy {  get; set; }
+        public string maDonVi {  get; set; }
+        public int idNhanVien {  get; set; }
+        public bool isGroup {  get; set; }
+        public bool disable { get; set;}
+
+        public classTaiKhoan() { }
+        public classTaiKhoan(int id, string userName, string password, string maCongTy, string maDonVi, int idNhanVien, bool isGroup, bool disable)
+        {
+            this.id = id;
+            this.userName = userName;
+            this.password = password;
+            this.maCongTy = maCongTy;
+            this.maDonVi = maDonVi;
+            this.idNhanVien = idNhanVien;
+            this.isGroup = isGroup;
+            this.disable = disable;
+        }
+    }
+}
---
+++ DataAccess/classThietBi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class classThietBi
+    {
+        public int id { get; set; }
+        public string tenThietBi {  get; set; }
+        public decimal dongGia {  get; set; }
+
+        public classThietBi() { }
+        public classThietBi(int id, string tenThietBi, decimal dongGia)
+        {
+            this.id = id;
+            this.tenThietBi = tenThietBi;
+            this.dongGia = dongGia;
+        }
+    }
+}
