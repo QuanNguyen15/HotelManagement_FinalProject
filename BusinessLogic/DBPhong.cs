5d597c2b962d49366418df9697fd8aa5ce4aa14d
viet <vietddls06@gmail.com>
2023-12-27 17:15:51 +0700

    viet 12/27 5:15

---
+++ BusinessLogic/DBPhong.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class DBPhong
+    {
+        cDB cDB = null;
+        DataTable dt = null;
+
+        public DBPhong() { }
+
+        public DBPhong(string sername, string dbname, string user = "", string pass = "")
+        {
+            cDB = new cDB(sername, dbname);
+
+        }
+
+        public DataTable getDataTable()
+        {
+            dt = new DataTable();
+            dt = cDB.getTable("Phong");
+
+            return dt;
+        }
+    }
+}
