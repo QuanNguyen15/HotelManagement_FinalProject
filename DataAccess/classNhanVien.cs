eaad3820e38b5f528891661840f4c60773b82dce
viet <vietddls06@gmail.com>
2023-12-27 16:54:54 +0700

    viet 4:54 12 27

---
+++ DataAccess/classNhanVien.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class classNhanVien
+    {
+        public int id { get; set; }
+        public string hoTen { get; set; }
+        public string soCCCD { get; set; }
+        public string dienThoai { get; set; }
+        public string email { get; set; }
+
+        public classNhanVien() { }
+        public classNhanVien(int id, string hoTen, string soCCCD, string dienThoai, string email)
+        {
+            this.id = id;
+            this.hoTen = hoTen;
+            this.soCCCD = soCCCD;
+            this.dienThoai = dienThoai;
+            this.email = email;
+        }
+    }
+}
---
+++ DataAccess/classPhongThietBi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class classPhongThietBi
+    {
+        public int idPhong { get; set; }
+        public int idThietBi { get; set; }
+        public int soLuong { get; set; }
+        public classPhongThietBi() { }
+
+        public classPhongThietBi(int idPhong, int idThietBi, int soLuong)
+        {
+            this.idPhong = idPhong;
+            this.idThietBi = idThietBi;
+            this.soLuong = soLuong;
+        }
+    }
+}
