5eb7523b0c16d54e5662b8c42ee275281108a953
viet <vietddls06@gmail.com>
2023-12-28 16:31:01 +0700

    viet12/28 4:30

--- BusinessLogic/BUSTaiKhoan.cs
+++ BusinessLogic/BUSTaiKhoan.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using DataAccess.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BusinessLogic
 {
-    internal class BUSTaiKhoan
+    public class BUSTaiKhoan
     {
+        ServerName ServerName = new ServerName();
+        DataTable dt = null;
+
+        public BUSTaiKhoan()
+        {
+        }
+
+        public DataTable getKhachHang()
+        {
+
+            DBTaiKhoan dBTaiKhoan = new DBTaiKhoan(ServerName.userName, "HOTELS");
+            DataTable getDatatable = dBTaiKhoan.getDataTable();
+
+            dt = getDatatable;
+            return dt;
+        }
     }
 }
--- BusinessLogic/BUSThietBi.cs
+++ BusinessLogic/BUSThietBi.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using DataAccess.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BusinessLogic
 {
-    internal class BUSThietBi
+    public class BUSThietBi
     {
+        ServerName ServerName = new ServerName();
+        DataTable dt = null;
+
+        public BUSThietBi()
+        {
+        }
+
+        public DataTable getKhachHang()
+        {
+
+            DBThietBi dBThietBi = new DBThietBi(ServerName.userName, "HOTELS");
+            DataTable getDatatable = dBThietBi.getDataTable();
+
+            dt = getDatatable;
+            return dt;
+        }
     }
 }
