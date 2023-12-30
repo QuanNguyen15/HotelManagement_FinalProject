9e92b410615adb12b25a5793214d6754a36059bb
viet <vietddls06@gmail.com>
2023-12-30 23:23:27 +0800

    viet 10:23

--- DataAccess/DAL/DBPhong.cs
+++ DataAccess/DAL/DBPhong.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using DataAccess;
@@ -29,5 +30,7 @@ namespace BusinessLogic
             dt = cDB.getTable("Phong");
             return dt;
         }
+
+       
     }
 }
