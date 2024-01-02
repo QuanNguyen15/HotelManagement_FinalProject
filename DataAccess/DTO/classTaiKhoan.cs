208213c4e6a2a7eb6ba0806b87461a12ee1b866a
viet <vietddls06@gmail.com>
2024-01-02 16:07:25 +0800

    viet 3:7

--- DataAccess/DTO/classTaiKhoan.cs
+++ DataAccess/DTO/classTaiKhoan.cs
@@ -11,23 +11,21 @@ namespace DataAccess
         public int id { get; set; }
         public string userName {  get; set; }
         public string password { get; set; }
-        public string maCongTy {  get; set; }
-        public string maDonVi {  get; set; }
+        public int idCongty {  get; set; }
+
         public int idNhanVien {  get; set; }
-        public bool isGroup {  get; set; }
-        public bool disable { get; set;}
+        public int idRole {  get; set; }
 
         public classTaiKhoan() { }
-        public classTaiKhoan(int id, string userName, string password, string maCongTy, string maDonVi, int idNhanVien, bool isGroup, bool disable)
+
+        public classTaiKhoan(int id, string userName, string password, int idCongty, int idNhanVien, int idRole)
         {
             this.id = id;
             this.userName = userName;
             this.password = password;
-            this.maCongTy = maCongTy;
-            this.maDonVi = maDonVi;
+            this.idCongty = idCongty;
             this.idNhanVien = idNhanVien;
-            this.isGroup = isGroup;
-            this.disable = disable;
+            this.idRole = idRole;
         }
     }
 }
